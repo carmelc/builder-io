95c598cf2a2ddea60f319f6535ecda98f78e1d81
carmelc <carmelc@wix.com>
2022-10-13 14:22:06 +0300

    attempt sdk data

--- plugins/wix-stores/src/domain/api.ts
+++ plugins/wix-stores/src/domain/api.ts
@@ -30,3 +30,42 @@ export function productsOperations(wixDomain: string): ProductOperations {
     }
     return {getAllProducts, getAllCollections};
 }
+
+// import { buildClient } from './client-builder';
+//
+// export interface ProductOperations {
+//     getAllProducts: (input?: GetAllProductsOpInput) => Promise<{products: any[]}>
+//     getAllCollections: (input?: GetAllProductsOpInput) => Promise<{collections: any[]}>
+// }
+//
+// export type GetAllProductsOpInput = {
+//     url?: string
+//     variables?: any
+//     preview?: boolean
+// };
+//
+// export function productsOperations(wixDomain: string): ProductOperations {
+//
+//     async function getAllProducts() {
+//         const client = await buildClient(wixDomain);
+//         return client.data.query({
+//             collectionName: "Stores/Products",
+//             includeReferencedItems: ["author"],
+//             dataQuery: {
+//                 paging: {limit: 500}
+//             }
+//         })
+//     }
+//
+//     async function getAllCollections() {
+//         const client = await buildClient(wixDomain);
+//         return client.data.query({
+//             collectionName: "Stores/Collections",
+//             includeReferencedItems: ["author"],
+//             dataQuery: {
+//                 paging: {limit: 500}
+//             }
+//         })
+//     }
+//     return {getAllProducts, getAllCollections};
+// }
--- plugins/wix-stores/src/domain/client-builder.ts
+++ plugins/wix-stores/src/domain/client-builder.ts
@@ -1,35 +1,18 @@
-import {API_URL, WIX_ACCESS_TOKEN_COOKIE, WIX_REFRESH_TOKEN_COOKIE, WIX_COOKIE_EXPIRE} from './consts'
+import {WIX_ACCESS_TOKEN_COOKIE, WIX_REFRESH_TOKEN_COOKIE, WIX_COOKIE_EXPIRE} from './consts'
 // @ts-ignore
 import Cookies from 'js-cookie';
 import {createClient, session} from "@wix/sdk";
 // @ts-ignore
-import { data } from '@wix/data-backend-public-sdk-poc'
-
-export function getError(errors: any[] | null, status: number) {
-    errors = errors ?? [{ message: 'Failed to fetch Wix API' }]
-    return new Error(`errors: ${errors}, status: ${status}`)
-}
-
-export async function getAsyncError(res: Response) {
-    const data = await res.json()
-    return getError(data.errors, res.status)
-}
-
-const handleFetchResponse = async (res: Response) => {
-    if (res.ok) {
-        return res.json();
-    }
-
-    throw await getAsyncError(res)
-}
+// import { data } from '@wix/data-backend-public-sdk-poc'
 
 const wixClient = createClient({
-    data,
+    data: {},
+    // data,
 })
 
 export type clientTypes = typeof wixClient
 
-const fetcher: any = async ({
+export const buildClient: any = async ({
                                 wixDomain,
                             }: any): Promise<clientTypes> => {
     let accessToken = Cookies.get(WIX_ACCESS_TOKEN_COOKIE) ?? ''
@@ -40,5 +23,3 @@ const fetcher: any = async ({
     wixClient.setSession(wixSession)
     return wixClient;
 }
-
-export default fetcher
